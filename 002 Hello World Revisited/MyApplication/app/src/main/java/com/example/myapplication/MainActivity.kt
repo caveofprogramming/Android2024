ccacf93ab31a83c0d66164215ebcd542912d08c3
John <john@Johns-iMac.station>
2024-08-23 18:09:07 +0200

    First commit

--- 002 Hello World Revisited/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ 002 Hello World Revisited/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,41 @@
-package com.example.android002_helloworldrevisited
+package com.example.example
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.android002_helloworldrevisited.ui.theme.Android002HelloWorldRevisitedTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Android002HelloWorldRevisitedTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Android002HelloWorldRevisitedTheme {
-        Greeting("Android")
+fun Greeting() {
+    Column(
+        modifier=Modifier.fillMaxSize().background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text="Hello", color= Color.Cyan, fontSize=40.sp)
+        Text(text="World", color=Color.Green, fontSize = 40.sp)
     }
 }
